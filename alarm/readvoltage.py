d1aea2c84d98d5b638cb92d89f1d79c3651c8cdf
Yipeng Sun <syp@umd.edu>
2018-04-25 17:57:13 -0400

    Test to read a single channel.

--- alarm/readvoltage.py
+++ alarm/readvoltage.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 #
-# Last Change: Mon Feb 05, 2018 at 09:11 PM -0500
+# Last Change: Wed Apr 25, 2018 at 05:56 PM -0400
 
 import RPi.GPIO as GPIO
 
 
-def init():
-    GPIO.setmode(GPIO.BOARD)
+class AlarmSetup(object):
+    ch1 = 14
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.ch1, pull_up_down=GPIO.PUD_DOWN)
+
+    def cleanup(self):
+        GPIO.cleanup()
+
+    def read_channels(self):
+        return GPIO.input(self.ch1)
 
 
-def cleanup():
-    GPIO.cleanup()
+if __name__ == '__main__':
+    from time import sleep
+
+    alarm = AlarmSetup()
+
+    while True:
+        try:
+            print(alarm.read_channels())
+            sleep(0.01)
+        except KeyboardInterrupt:
+            break
