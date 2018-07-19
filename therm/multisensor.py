192bc931d2189ff763c4378018ee0ead4398afab
Jorge Ramirez <jorger99@terpmail.umd.edu>
2018-07-19 15:01:22 -0400

    second edition of multisensor.py

--- therm/multisensor.py
+++ therm/multisensor.py
@@ -1,93 +1,88 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # Author: Jorge Ramirez
-# Last Change: Thu Jul 12, 2018 at 04:56 PM -0400
+# Last Change: Thu Jul 19, 2018 at 12:07 PM -0400
 
 import threading
 import time
-# FIXME: use pathlib instead of os.path---it's a newer lib with nicer features.
+ # FIXME: use pathlib instead of os.path---it's a newer lib with nicer features.
 import os.path
 
-# FIXME: don't use global variables---these should be configurable
-# delay = 1  # 0.75 second delay between prints
-# default_path = '/sys/bus/w1/devices'
+ # list of current 9 sensors
+sensor_dir = ["8d0cbe", "8d62d7", "8d8197", "8d94eb", "8dd2b8",
+"8e7ed7", "8f007f", "8fd7e3", "9049c6"]
+
+ # create directory of threads to be called
+thread_dir = ["Thread" + str(x + 1) for x in range(9)]
 
 
-# Detect how many sensors there are start 1 thread for each sensor
-# FIXME: need implementation
+ # FIXME: need implementation
 def get_total_sensor_num(path):
+     # Detect how many sensors there are & start 1 thread for each sensor
     pass
 
 
 class ThermThread(threading.Thread):
     '''
-    each thermThread initializes w/ "ThreadID" which will be integer used as
-    identifier "name" which will be therm serial.
+    each thermThread initializes w/ "thread_id" which will be an integer used
+    as identifier "name" which will be therm serial.
     '''
+
+     # initialize thread with standard arguments
     def __init__(self,
-                 thread_id, sensor, *args,
-                 delay=1, default_path='/sys/bus/w1/devices', **kwargs):
+        thread_id, sensor, *args,
+        delay=1, default_path='/sys/bus/w1/devices', **kwargs):
+
         self.thread_id = thread_id
         self.sensor = sensor
-
         self.delay = delay
         self.default_path = default_path
 
-        super().__init__(self, *args, **kwargs)
+        super(ThermThread, self, *args, **kwargs).__init__()
 
-    # default run will be to announce start print sensor name & temperature
-    # continuously
-    def run(self):
-        print(("Starting Thread " + str(self.threadID) +
-        " sensor " + self.sensor))
-        while True:
-            temp = self.get_therm(self.sensor)
-            self.print_therm(self.sensor, temp, self.thread_id)
+     # thread invited to introduce themselves
+    def announce(self):
+        print(("Starting: (Thread " + str(self.thread_id) + ") (" +
+        self.sensor + ")"))
 
-    def get_therm(self):
+     # thread opens temperature file and then saves it as a string
+    def extract_therm(self):
         newpath = os.path.join(self.default_path, "28-000009" + self.sensor,
         'w1_slave')
+         # open file, copy all contents, extract data
+        with open(newpath, 'r') as file:
+                contents = file.readlines()
+                time.sleep(self.delay)
+                 # extract raw data into variable "temp_string"
+                temp_output = contents[1].find('t=')
+                temp_string = contents[1].strip()[temp_output + 2:]
+        return temp_string
 
-        # open file, copy all contents, close file
-        # FIXME: use 'with'
-        temp_file = open(newpath, 'r')
-        contents = temp_file.readlines()
-        temp_file.close()
-        time.sleep(self.delay)
-
-        # extract temperature
-        temp_output = contents[1].find('t=')  # look for temp inside file
-        temp_string = contents[1].strip()[temp_output + 2:]  # strip temp out
-        temp_c = (float(temp_string) / 1000.0 * 9 / 5.0) + 32.0
-
-        return temp_c
+     # simple convert method. It will probably be removed. perhaps it will be
+     # more beneficial to return the raw temp_string to the server
+    def convert_therm(self, sensor_num):
+        fahrenheit = (float(sensor_num) / 1000.0 * 9 / 5.0) + 32.0
+        return fahrenheit
 
     @staticmethod
-    def print_therm(sensor, data, thread_id):
-        print("Sensor {} ({}) detects {}".format(thread_id, sensor, data))
+    def print_therm(self, sensor, data, thread_id):
+        print(("Sensor {} ({}) detects {}".format(thread_id, sensor, data)))
 
+     # by default, each thread will
+     # announce -> extract -> convert -> print
+
+    def run(self):
+        self.announce()  # announce
+        while True:
+            data = self.extract_therm()  # extract
+            final = self.convert_therm(data)  # convert
+            self.print_therm(self, self.sensor, final, self.thread_id)  # print
 
 if __name__ == '__main__':
-    # FIXME: non-idomaitc implementation. Use a for loop.
-    # create new threads
-    thread1 = ThermThread(1, "8d0cbe")
-    thread2 = ThermThread(2, "8d62d7")
-    thread3 = ThermThread(3, "8d8197")
-    thread4 = ThermThread(4, "8d94eb")
-    thread5 = ThermThread(5, "8dd2b8")
-    thread6 = ThermThread(6, "8e7ed7")
-    thread7 = ThermThread(7, "8f007f")
-    thread8 = ThermThread(8, "8fd7e3")
-    thread9 = ThermThread(9, "9049c6")
-    # start new threads
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    thread4.start()
-    thread5.start()
-    thread6.start()
-    thread7.start()
-    thread8.start()
-    thread9.start()
 
-    # FIXME: need to wait for sensor threads to join in the end
-    thread1.join()  # etc...
+     # create new threads
+    for i in range(len(sensor_dir)):
+        thread_dir[i] = ThermThread(i, sensor_dir[i])  # (thread_id, sensor)
+
+     # start new threads once all have been initialized
+    for i in range(len(sensor_dir)):
+        thread_dir[i].start()
