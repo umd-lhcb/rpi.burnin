320602ffcd72194a67a5ce3dc3597c673cbfdc7a
Jorge Ramirez <jorger99@terpmail.umd.edu>
2018-08-02 12:32:53 -0400

    I forgot to commit -a... whoops

--- moist/leaksensor2.0.py
+++ moist/leaksensor2.0.py
@@ -1,40 +1,50 @@
-# Code for fire alarm leak sensor
-# Written by Jorge Ramirez Ortiz
-# Last edit: Thu Aug 2, 2018 at 10:50 PM -0400
-
- # 2.0 uses a different event detection method
-
- # nominal: no water means circuit is open & BCM 17 is LOW due to PULLUP
- # leak detected: leak sensor closes, becomes 2Mohm resistor & BCM 17 is HIGH
-
+                  #!/usr/bin/env python
+#
+# Last Change: Thu Apr 26, 2018 at 02:08 PM -0400
 
 import RPi.GPIO as GPIO
 import time
+import sys
 
- # set up
 InputBCM = 17
-GPIO.setmode(GPIO.BCM)  # use BCM pin system
-GPIO.setup(InputBCM, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # activate pin
-
- # this method will be called if GPIO detects circuit closing
 
 
-def sound_the_alarm(channel):
-    time.sleep(0.5)
-    if GPIO.input(channel) == GPIO.HIGH:
-        print('Leak! Leak!')
+class AlarmSetup(object):
+    def __init__(self, ch):
+        self.ch = ch
+
+        # Use the Board numbering
+        GPIO.setmode(GPIO.BOARD)
+
+        # To quote from:
+        # https://www.raspberrypi.org/forums/viewtopic.php?t=87292
+        #   The pull-up/downs supply some voltage so that the GPIO will have a
+        #   defined value UNTIL overridden by a stronger force.
+        #   You should set a pull-down (to 0) when you expect the stronger force
+        #   to pull it up to 1.
+        #   You should set a pull-up (to 1) when you expect the stronger force
+        #   to pull it down to 0.
+        #   Maybe we don't need this parameter after all.
+        #GPIO.setup(self.ch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.ch, GPIO.IN)
+
+    def cleanup(self):
+        GPIO.cleanup()
+
+    def read_channel(self):
+        return GPIO.input(self.ch)
 
 
- # main leak detection
-try:
-    GPIO.add_event_detect(InputBCM, GPIO.BOTH, callback=sound_the_alarm)
+if __name__ == '__main__':
 
-     # add_event_detect runs as a separate thread that waits for the pin to
-     # change states, and when it does, it executes sound_the_alarm
+    # alarm = AlarmSetup(ch=int(sys.argv[1]))
+    alarm = WaterAlarm(InputBCM)
+    while True:
+        try:
+            if alarm.read_channel() == 0:
+                print('Fire!')
+                time.sleep(0.25)
+        except KeyboardInterrupt:
+            break
 
-    message = input('\nPress Any Key to Exit.\n')
-
-finally:
-    GPIO.cleanup()
-
-print("Exiting")
+    alarm.cleanup()
