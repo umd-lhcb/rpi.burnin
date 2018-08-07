b40bddc7297d44f1c7a757ec61ad9c4a054fbe1f
Jorge Ramirez <jorger99@terpmail.umd.edu>
2018-08-07 09:20:34 -0400

    quick new .py file for testing new circuit

--- moist/leaksensor.py
+++ moist/leaksensor.py
@@ -1,40 +1,43 @@
-# Code for fire alarm leak sensor
+# Code for firealarm leak sensor
 # Written by Jorge Ramirez Ortiz
-# Last edit: Tue Jul 31, 2018 at 13:50 PM -0400
+# Last edit: Tue Aug 07, 2018 at 9:01 AM -0400
 
  # nominal: no water means circuit is open & BCM 17 is LOW
  # leak detected: leak sensor closes, becomes 2Mohm resistor & BCM 17 is HIGH
 
-
 import RPi.GPIO as GPIO
 import time
-
- # set up
-InputBCM = 17
-GPIO.setmode(GPIO.BCM)  # use BCM pin system
-GPIO.setup(InputBCM, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # setup pin w/ pullup
+import sys
 
 
- # this method will be called if BCM 17 is HIGH (i.e. circuit closed)
-def sound_the_alarm(channel):
-    if GPIO.input(channel) == GPIO.HIGH:
-        print('Leak! Leak!')
-        time.sleep(0.01)
+class WaterAlarm(object):
+
+    def __init__(self, ch):
+        self.ch = ch
+        self.leakcounter = 0  # counter to see how long leak lasted
+        GPIO.setmode(GPIO.BCM)  # use BCM pin system
+        GPIO.setup(self.ch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # activate pin
+
+    def cleanup(self):
+        GPIO.cleanup(self.ch)
+
+    def read_channel(self):
+        return GPIO.input(self.ch)
+
+    def hitcounter(self):
+        self.leakcounter += 1
+        return str(self.leakcounter)
 
 
- # use event detect to monitor BCM17
-GPIO.add_event_detect(InputBCM, GPIO.BOTH, callback=sound_the_alarm,
-    bouncetime=100)
+if __name__ == '__main__':
+    print('initializing WaterAlarm')
+    alarm = WaterAlarm(ch=int(sys.argv[1]))  # init alarm w/ ch from terminal
 
- # add_event_detect runs as a separate thread that waits for the pin to
- # change states, and when it does, it executes sound_the_alarm
-
-
-try:
-     # keep script open with a way to exit
-    message = input('\nEnter Any Key to Exit.\n')
-
-finally:
-    GPIO.cleanup()
-
-print("Exiting")
+    while True:
+        try:
+            if alarm.read_channel() == 1:
+                time.sleep(0.1)
+                print('Leak detected! (' + alarm.hitcounter() + ')')
+        except KeyboardInterrupt:
+            alarm.cleanup()
+            break
