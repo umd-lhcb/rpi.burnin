367042a2be1a9f9a389ff69173fbcf3b4ea55f20
Yipeng Sun <syp@umd.edu>
2018-08-15 17:54:50 -0400

    Make water detector run inside a separate thread.

--- water/WaterAlarm.py
+++ water/WaterAlarm.py
@@ -1,57 +1,73 @@
 #!/usr/bin/env python
 #
 # Authors: Jorge Ramirez Ortiz, Yipeng Sun
-# Last Change: Wed Aug 15, 2018 at 03:37 PM -0400
+# Last Change: Wed Aug 15, 2018 at 05:54 PM -0400
 
-# nominal: no water means circuit is open & BCM 17 is LOW due to PULLDOWN
-# water detected: leak sensor closes, becomes 2Mohm resistor & BCM 17 is HIGH
 
 import RPi.GPIO as GPIO
-import time
 import sys
 
+from threading import Thread, Event
 
-class WaterAlarm(object):
-    '''
-    alarm.leakcounter variable can be used to guard against false alarms.
 
-    This script will monitor the input pin for a HIGH. it is default LOW due to
-    an internal pulldown resistor. it will probe every 0.1 seconds, and if it
-    detects a leak, then the 'leakcounter' variable wil lincrease by one.
-
-    It seems as if 1 hit to leakcounter can be safely ignored, even tiny drops
-    that run across the sensor trigger the sensor at least twice.
-
-    Anything more than 5 hits is a major leak (the sensor is in a puddle of
-    water)
-
-    '''
-    def __init__(self, ch):
+class WaterAlarm(Thread):
+    def __init__(self, stop_event, *args,
+                 ch=9, interval=0.01, alarm_threshhold=2, **kwargs):
         self.ch = ch
-        self.leakcounter = 0  # counter to see how long leak lasted
-        GPIO.setmode(GPIO.BCM)  # use BCM pin system
+        self.interval = interval
+
+        # 'leak_counter' variable can be used to guard against false alarms.
+        #
+        # This class will monitor the input pin for a HIGH. It is default LOW
+        # due to an internal pulldown resistor. It will probe every 0.1 seconds,
+        # and if it detects a leak, then the 'leak_counter' variable will
+        # increase by one.
+        #
+        # It seems as if 1 hit to leak_counter can be safely ignored, even tiny
+        # drops that run across the sensor trigger the sensor at least twice.
+        #
+        # Anything more than 5 hits is a major leak (the sensor is in a puddle
+        # of water)
+        self.leak_counter = 0
+
+        # nominal: no water means circuit is open & BCM 17 is LOW due to
+        #          PULLDOWN
+        # water detected: leak sensor closes, becomes 2Mohm resistor & BCM 17 is
+        #                 HIGH
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.ch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # activate pin
+
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        while not self.stop_event.wait(self.interval):
+            if self.read_channel() == 1:
+                self.leak_counter += 1
+                if self.leak_counter >= self.alarm_threshhold:
+                    self.alarm()
 
     def cleanup(self):
         GPIO.cleanup(self.ch)
+        self.join()
 
     def read_channel(self):
         return GPIO.input(self.ch)
 
-    def hitcounter(self):
-        self.leakcounter += 1
-        return str(self.leakcounter)
+    def alarm(self):
+        print('Leak!!!')
 
 
-if __name__ == '__main__':  # ensure that script is being run from termianl
-    print('initializing WaterAlarm')
-    alarm = WaterAlarm(ch=int(sys.argv[1]))  # init alarm w/ ch from terminal
+if __name__ == '__main__':  # ensure that script is being run from terminal
+    stop_event = Event()
+
+    print("Initializing water leak detector...")
+    alarm = WaterAlarm(stop_event, ch=int(sys.argv[1]))
+    alarm.start()
 
     while True:
         try:
-            if alarm.read_channel() == 1:
-                time.sleep(0.1)
-                print('Leak detected! (' + alarm.hitcounter() + ')')
+            pass
         except KeyboardInterrupt:
-            alarm.cleanup()
-            break
+            stop_event.set()
+
+    alarm.cleanup()
