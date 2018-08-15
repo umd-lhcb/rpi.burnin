da9176136ec78e61d83cd3a293bd885a3d9444a8
Yipeng Sun <syp@umd.edu>
2018-08-15 13:02:57 -0400

    Various cleanups.

--- therm/thermsensor.py
+++ therm/thermsensor.py
@@ -1,13 +1,62 @@
 # !/usr/bin/env python
 #
 # Authors: Jorge Ramirez, Yipeng Sun
-# Last Change: Wed Aug 15, 2018 at 10:30 AM -0400
+# Last Change: Wed Aug 15, 2018 at 01:02 PM -0400
 
-import threading
+import logging
 import time
 import sys
 
 from pathlib import Path
+from threading import Thread
+
+logger = logging.getLogger(__name__)
+
+
+class ThermalSensor(Thread):
+    def __init__(self, *args,
+                 sensor=None, display_name=None, interval=5,
+                 **kwargs):
+        self.sensor = sensor
+        self.display_name = display_name
+        self.interval = interval
+
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        self.announce()
+
+        while True:
+            try:
+                data = str(self.get())
+                self.print_therm(self.sensor.stem, self.display_name, data)
+                time.sleep(self.interval)
+            except KeyboardInterrupt:
+                break
+
+        self.cleanup()
+
+    def get(self):
+        with self.sensor.open() as f:
+            contents = f.readlines()
+            # extract raw data into variable "temp_string"
+            temp_output = contents[1].find('t=')
+            temp_string = contents[1].strip()[temp_output + 2:]
+
+        return int(temp_string) / 1000  # add decimal point to data
+
+    def cleanup(self):
+        self.join()
+
+    def announce(self):
+        logger.info("Starting: read from {}, with a display name of {}".format(
+            self.sensor_path, self.display_name
+        ))
+
+    @staticmethod
+    def print_therm(sensor_name, display_name, data):
+        print(("Sensor {} (from file {}) detects {}".format(
+            display_name, sensor_name, data)))
 
 
 ###########
@@ -15,92 +64,40 @@ from pathlib import Path
 ###########
 
 def detect_sensors(sensor_dir="/sys/bus/w1/devices",
-                   sensor_name_prefix='28-00000'):
+                   sensor_name_prefix='28-00000',
+                   sensor_file_name='w1_slave'):
     scan_dir = Path(sensor_dir)  # set directory to be scanned
     sensor_list = []
 
     print('Detecting sensors and adding to list...')
     for item in scan_dir.iterdir():
         if item.is_dir() and item.stem[:8] == sensor_name_prefix:
-            sensor_list.append(item)
-            print('sensor {} appended.'.format(item.stem))
+            sensor = item + Path(sensor_file_name)
+            sensor_list.append(sensor)
+            print('sensor {} appended.'.format(sensor.stem))
 
     return sensor_list
 
 
-def give_sensor_path(index):
-    return sensorfolders[index]
+def list_all_sensors(**kwargs):
+    sensor_list = detect_sensors(**kwargs)
 
+    for sensor in sensor_list:
+        print("Detected the following sensor: {}".format(sensor.stem))
 
-def give_sensor_serial(index):
-    return str(sensorfolders[index].stem)[8:]
-
-
-def list_all_sensors():
-    print((str(len(sensorfolders)) + ' sensors found:'))
-    for item in range(len(sensorfolders)):
-        print((give_sensor_serial(item)))
-
-
-class ThermalThread(threading.Thread):
-
- # newthread = ThermalThread(thread_id, sensor serial #, delay (seconds))
-    def __init__(self, thread_id, sensor, delay, *args, **kwargs):
-        self.thread_id = thread_id
-        self.sensor = sensor
-        self.delay = delay
-
-        if self.delay is None:
-            self.delay = 1
-        else:
-            self.delay = self.delay
-
-        super(ThermalThread, self, *args, **kwargs).__init__()
-
-     # thread announces itself
-    def announce(self):
-        print(("Starting: (Thread " + str(self.thread_id) + ") (" +
-        str(self.sensor) + ")"))
-
-    def extract_therm(self):
-        sensordir = give_sensor_path(self.thread_id)  # get directory of sensor
-        path = sensordir / 'w1_slave'  # append file to path
-
-             # open file, copy all contents, extract data
-        with path.open() as f:
-                contents = f.readlines()
-                time.sleep(self.delay)
-                 # extract raw data into variable "temp_string"
-                temp_output = contents[1].find('t=')
-                temp_string = contents[1].strip()[temp_output + 2:]
-
-        return (int(temp_string) / 1000)  # add decimal point to data
-
-    @staticmethod
-    def print_therm(self, sensor, data, thread_id):
-        serial = give_sensor_serial(thread_id)
-        print(("Sensor {} ({}) detects {}".format(thread_id, serial, data)))
-
-    def run(self):
-        self.announce()  # announce init
-        while True:
-            data = str(self.extract_therm())  # extract
-            self.print_therm(self, self.sensor, data, self.thread_id)  # print
-
-
- # main code
 
 if __name__ == '__main__':
+    # detect sensors and assign threads
+    sensor_path = detect_sensors()
+    sensor_list = []
 
-     # detect sensors and assign threads
-    detect_sensors()
-    thread_dir = ["Thread" + str(x + 1) for x in range(len(sensorfolders))]
+    # create new threads
+    for i in range(len(sensor_path)):
+        sensor_list.append(
+            ThermalSensor(sensor=sensor_path[i], display_name=str(i),
+                          interval=int(sys.argv[1])
+                          ))
 
-     # create new threads
-    for i in range(len(sensorfolders)):
-        thread_dir[i] = ThermalThread(i, sensorfolders[i],
-        int(sys.argv[1]))  # delay is input via terminal, default is 1s
-
-     # start new threads once all have been initialized
-    for i in range(len(sensorfolders)):
-        thread_dir[i].start()
+    # start new threads once all have been initialized
+    for sensor in sensor_list:
+        sensor.start()
