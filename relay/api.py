fadce1e8f820d922012c5958facdec71ff149142
Yipeng Sun <syp@umd.edu>
2018-11-06 12:27:27 -0500

    Find the {vendor,product} id of the USB relay.

---
+++ relay/api.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+# Authors: Yipeng Sun
+# Last Change: Tue Nov 06, 2018 at 12:24 PM -0500
+
+# These are found from 'lsusb', then convert the hex numbers to dec.
+# For each entry, it is formatted as: (vendor_id, product_id)
+RELAY_ID = ((5824, 1503))
