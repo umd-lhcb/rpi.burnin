83f4ff0bb3752b3a187def0fcdb7c27d729d621b
Yipeng Sun <syp@umd.edu>
2018-11-07 16:58:59 -0500

    Now we can also set the alias (persistent) of the relay.

--- relay/api.py
+++ relay/api.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 #
 # Authors: Yipeng Sun
-# Last Change: Wed Nov 07, 2018 at 04:38 PM -0500
+# Last Change: Wed Nov 07, 2018 at 04:57 PM -0500
 
 import hid
 
 # These are found from 'lsusb'; it is formatted as: (vendor_id, product_id)
 RELAY_ID = (0x16c0, 0x05df)
+CMD_SET_ALIAS = 0xfa
+ON = 0xff
+OFF = 0xfd
 
 
 #################################
@@ -40,10 +43,14 @@ def set_device_alias(path, alias):
     if len(alias) > 5:
         raise ValueError('The length of the alias should not exceed 5.')
     else:
+        cmd = [0, CMD_SET_ALIAS]
+        cmd += ord_str(alias)
+        cmd += [0] * (9-len(cmd))
+
         dev = hid.device()
 
         dev.open_path(path)
-        alias = chr_list(dev.get_feature_report(0, 9)[1:6])
+        dev.send_feature_report(cmd)
         dev.close()
 
 
