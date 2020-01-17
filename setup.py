98efd267275f1ad9d11ad0ed531ca647e73bbd0b
Yipeng Sun <syp@umd.edu>
2020-01-17 05:53:55 -0500

    Add a simplet setup.py.

---
+++ setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='rpi.burnin',
+    version='0.3.0',
+    description='Rasberry Pi Python modules for burn-in ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/umd-lhcb/rpi.burnin',
+
+    author='UMD LHCb group',
+    author_email='lhcb@physics.umd.edu',
+
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # These classifiers are *not* checked by 'pip install'. See instead
+        # 'python_requires' below.
+        'Programming Language :: Python :: 3',
+    ],
+
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    # package_dir={'': 'src'},  # Optional
+
+    packages=find_packages('rpi.burnin'),  # Required
+    python_requires='>=3, <4',
+
+    install_requires=['RPi.GPIO'],
+    # extras_require={  # Optional
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+    # package_data={  # Optional
+    #     'sample': ['package_data.dat'],
+    # },
+)
