839f884006819998a2361c07cd4d0d29a6a755aa
Yipeng Sun <syp@umd.edu>
2020-01-17 06:36:54 -0500

    Clean up setup.py.

--- setup.py
+++ setup.py
@@ -29,39 +29,15 @@ setup(
     author='UMD LHCb group',
     author_email='lhcb@physics.umd.edu',
 
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
+    classifiers=[
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-
-        # Pick your license as you wish
         'License :: OSI Approved :: BSD License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        # These classifiers are *not* checked by 'pip install'. See instead
-        # 'python_requires' below.
         'Programming Language :: Python :: 3',
     ],
 
-    # When your source code is in a subdirectory under the project root, e.g.
-    # `src/`, it is necessary to specify the `package_dir` argument.
-    # package_dir={'': 'src'},  # Optional
-
-    packages=find_packages('rpi.burnin'),  # Required
+    packages=find_packages('rpi.burnin'),
     python_requires='>=3, <4',
 
-    install_requires=['RPi.GPIO'],
-    # extras_require={  # Optional
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
-    # package_data={  # Optional
-    #     'sample': ['package_data.dat'],
-    # },
+    install_requires=['RPi.GPIO']
 )
