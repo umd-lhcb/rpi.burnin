4761bb93a9899d701c343532222c6753f2fb93b2
Yipeng Sun <syp@umd.edu>
2020-07-20 01:30:20 +0800

    Add a simple fire alarm CPU usage tester.

---
+++ bin/FireAlarmSimple.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# Authors: Yipeng Sun
+
+import sys
+
+from time import sleep
+
+try:
+    import RPi.GPIO as GPIO
+except (ModuleNotFoundError, ImportError):
+    import fake_rpi
+
+    sys.modules["RPi"] = fake_rpi.RPi
+    sys.modules["smbus"] = fake_rpi.smbus
+
+    import RPi.GPIO as GPIO
+
+
+if __name__ == "__main__":  # ensure that script is being run from terminal
+    ch = int(sys.argv[1])
+    interval = 0.1
+
+    GPIO.setup(ch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    try:
+        while True:
+            readout = GPIO.input(ch)
+            if readout == 0:
+                print("Fire!")
+            sleep(interval)
+
+    except KeyboardInterrupt:
+        pass
