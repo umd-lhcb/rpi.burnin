e65f0157f367fe463f03eb6e2e32d52960b9df12
Yipeng Sun <syp@umd.edu>
2020-07-20 01:22:50 +0800

    Remove loading of external Python venv.

--- shell.nix
+++ shell.nix
@@ -13,30 +13,8 @@ pkgs.mkShell {
 
     # Some Python libraries needs to be installed via nix
     hidapi
-
-    # Python requirements (enough to get a virtualenv going).
-    virtualenvwrapper
   ]
   ++ stdenv.lib.optionals (stdenv.isDarwin) [ fake_rpi ]
   ++ stdenv.lib.optionals (!stdenv.isDarwin) [ RPi_GPIO ]
   ;
-
-  shellHook = ''
-    # Allow the use of wheels.
-    SOURCE_DATE_EPOCH=$(date +%s)
-
-    if test -d $HOME/build/python-venv; then
-      VENV=$HOME/build/python-venv/rpi.burnin
-    else
-      VENV=./.virtualenv
-    fi
-
-    if test ! -d $VENV; then
-      virtualenv $VENV
-    fi
-    source $VENV/bin/activate
-
-    # allow for the environment to pick up packages installed with virtualenv
-    export PYTHONPATH=$VENV/${python.sitePackages}/:$PYTHONPATH
-  '';
 }
